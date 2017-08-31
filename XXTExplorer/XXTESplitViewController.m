2a69a631400dad9e632a8bfec528688c2d8cd396
Lessica <i.82@me.com>
2017-08-31 17:35:54 +0800

    fix appearance init position

--- XXTExplorer/XXTESplitViewController.m
+++ XXTExplorer/XXTESplitViewController.m
@@ -36,45 +36,7 @@
 }
 
 - (void)setupAppearance {
-    LGAlertView *alertAppearance = [LGAlertView appearanceWhenContainedIn:[self class], nil];
-    alertAppearance.coverColor = [UIColor colorWithWhite:1.0 alpha:0.25];
-    XXTE_START_IGNORE_PARTIAL
-    if (XXTE_SYSTEM_8) {
-        alertAppearance.coverBlurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-    }
-    XXTE_END_IGNORE_PARTIAL
-    alertAppearance.coverAlpha = 0.85;
-    alertAppearance.layerShadowColor = [UIColor colorWithWhite:0.0 alpha:0.3];
-    alertAppearance.layerShadowRadius = 4.0;
-    alertAppearance.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1.0];
-    alertAppearance.buttonsHeight = 44.0;
-    alertAppearance.titleFont = [UIFont boldSystemFontOfSize:18.0];
-    alertAppearance.titleTextColor = [UIColor blackColor];
-    alertAppearance.messageTextColor = [UIColor blackColor];
-    alertAppearance.activityIndicatorViewColor = XXTE_COLOR;
-    alertAppearance.buttonsTitleColor = XXTE_COLOR;
-    alertAppearance.buttonsBackgroundColorHighlighted = XXTE_COLOR;
-    alertAppearance.cancelButtonTitleColor = XXTE_COLOR;
-    alertAppearance.cancelButtonBackgroundColorHighlighted = XXTE_COLOR;
-    alertAppearance.destructiveButtonTitleColor = XXTE_COLOR_DANGER;
-    alertAppearance.destructiveButtonBackgroundColorHighlighted = XXTE_COLOR_DANGER;
-    alertAppearance.progressLabelFont = [UIFont italicSystemFontOfSize:14.f];
-    alertAppearance.progressLabelLineBreakMode = NSLineBreakByTruncatingHead;
-    alertAppearance.dismissOnAction = NO;
-    alertAppearance.buttonsIconPosition = LGAlertViewButtonIconPositionLeft;
-    alertAppearance.buttonsTextAlignment = NSTextAlignmentLeft;
-
-    [XXTEToastManager setTapToDismissEnabled:YES];
-    [XXTEToastManager setDefaultDuration:2.f];
-    [XXTEToastManager setQueueEnabled:NO];
-    [XXTEToastManager setDefaultPosition:XXTEToastPositionCenter];
-
-    XXTEToastStyle *toastStyle = [XXTEToastManager sharedStyle];
-    toastStyle.backgroundColor = [UIColor colorWithWhite:0.f alpha:.6f];
-    toastStyle.titleFont = [UIFont boldSystemFontOfSize:14.f];
-    toastStyle.messageFont = [UIFont systemFontOfSize:14.f];
-    toastStyle.activitySize = CGSizeMake(80.f, 80.f);
-    toastStyle.verticalMargin = 16.f;
+    
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
