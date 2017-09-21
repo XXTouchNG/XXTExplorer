af8504cf3227bb1c89e0235a5d337dd8ff46e565
苏泽 <havonz@me.com>
2017-09-22 03:37:54 +0800

    LuaNSValue：对 table -> NSValue 做了最大深度限制

--- XXTPickerCollection/LuaNSValue.h
+++ XXTPickerCollection/LuaNSValue.h
@@ -6,40 +6,49 @@
 
 #ifdef __cplusplus
 
-	#import "lua.hpp"
+        #import "lua.hpp"
 
 #else
 
-	#import "lua.h"
-	#import "lualib.h"
-	#import "lauxlib.h"
+        #import "lua.h"
+        #import "lualib.h"
+        #import "lauxlib.h"
 
 #endif
 
 #ifdef __cplusplus
-	extern "C" {
+        extern "C" {
 #endif
-        extern int lua_table_is_array(lua_State *L, int index);
 
-        extern void lua_pushNSDictionary(lua_State *L, NSDictionary *dict);
-        extern void lua_pushNSArray(lua_State *L, NSArray *arr);
-        extern void lua_pushNSValue(lua_State *L, id value);
+        int lua_table_is_array(lua_State *L, int index);
 
-        extern NSDictionary *lua_toNSDictionaryx(lua_State *L, int index, NSMutableDictionary *result);
-        extern NSArray *lua_toNSArrayx(lua_State *L, int index, NSMutableArray *result);
-        extern id lua_toNSValue(lua_State *L, int index);
+        void lua_pushNSDictionaryx(lua_State *L, NSDictionary *dict, int level);
+        void lua_pushNSArrayx(lua_State *L, NSArray *arr, int level);
+        void lua_pushNSValuex(lua_State *L, id value, int level);
 
-        extern int luaopen_json(lua_State *L);
-        extern int luaopen_plist(lua_State *L);
-        extern void lua_openNSValueLibs(lua_State *L);
+        NSDictionary *lua_toNSDictionaryx(lua_State *L, int index, NSMutableDictionary *result, int level);
+        NSArray *lua_toNSArrayx(lua_State *L, int index, NSMutableArray *result, int level);
+        id lua_toNSValuex(lua_State *L, int index, int level);
 
-        extern NSString * const kXXTELuaVModelErrorDomain;
-        extern BOOL checkCode(lua_State *L, int code, NSError **error);
+        int luaopen_json(lua_State *L);
+        int luaopen_plist(lua_State *L);
+        void lua_openNSValueLibs(lua_State *L);
+
+        NSString * const kXXTELuaVModelErrorDomain;
+        BOOL checkCode(lua_State *L, int code, NSError **error);
+
 #ifdef __cplusplus
-	}
+        }
 #endif
 
-#define lua_toNSDictionary(L, IDX) lua_toNSDictionaryx((L), (IDX), nil)
-#define lua_toNSArray(L, IDX) lua_toNSArrayx((L), (IDX), nil)
+#define lua_pushNSDictionary(L, V) lua_pushNSDictionaryx((L), (V), 0)
+#define lua_pushNSArray(L, V) lua_pushNSArrayx((L), (V), 0)
+#define lua_pushNSValue(L, V) lua_pushNSValuex((L), (V), 0)
+
+#define lua_toNSDictionary(L, IDX) lua_toNSDictionaryx((L), (IDX), nil, 0)
+#define lua_toNSArray(L, IDX) lua_toNSArrayx((L), (IDX), nil, 0)
+#define lua_toNSValue(L, IDX) lua_toNSValuex((L), (IDX), 0)
+
+#define LUA_NSVALUE_MAX_DEPTH 100
 
 #endif
