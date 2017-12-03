d67f165de22f3fa2b4323060438ca52b6e716643
Lessica <i.82@me.com>
2017-12-04 00:17:26 +0800

    Fix bugs

---
+++ XXTExplorer/Master/Explorer/Extensions/XXTExplorerViewController+XXTExplorerCreateItemViewControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  XXTExplorerViewController+XXTExplorerCreateItemViewControllerDelegate.h
+//  XXTExplorer
+//
+//  Created by Zheng on 03/12/2017.
+//  Copyright © 2017 Zheng. All rights reserved.
+//
+
+#import "XXTExplorerViewController.h"
+
+@interface XXTExplorerViewController (XXTExplorerCreateItemViewControllerDelegate)
+
+@end
---
+++ XXTExplorer/Master/Explorer/Extensions/XXTExplorerViewController+XXTExplorerCreateItemViewControllerDelegate.m
@@ -0,0 +1,13 @@
+//
+//  XXTExplorerViewController+XXTExplorerCreateItemViewControllerDelegate.m
+//  XXTExplorer
+//
+//  Created by Zheng on 03/12/2017.
+//  Copyright © 2017 Zheng. All rights reserved.
+//
+
+#import "XXTExplorerViewController+XXTExplorerCreateItemViewControllerDelegate.h"
+
+@implementation XXTExplorerViewController (XXTExplorerCreateItemViewControllerDelegate)
+
+@end
