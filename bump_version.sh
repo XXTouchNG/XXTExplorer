34670042aa9b6a5f843d65db56a88180083a1b63
Lessica <i.82@me.com>
2017-12-25 21:37:49 +0800

    plutil

--- bump_version.sh
+++ bump_version.sh
@@ -3,6 +3,31 @@
 BVER="${1}"
 
 find . -name "*.plist" -exec plutil -lint {} \;
+
+# 3
 plutil -replace "DAEMON_VERSION" -string "${BVER}" "XXTExplorer/Defines/XXTEAppDefines.plist"
 plutil -replace "DAEMON_VERSION" -string "${BVER}" "XXTExplorer/Defines/XXTEAppDefines.AppStore.plist"
-plutil -replace "items.1" -xml "<dict><key>cell</key><string>About</string><key>label</key><string>XXTouch Editor\nv1.2-4</string><key>value</key><string>https://www.xxtouch.com\n2016-2017 © XXTouch Team.\nAll Rights Reserved.</string></dict>"
+
+# 4
+plutil -remove "items.1" "Settings.bundle/About.plist"
+plutil -remove "items.1" "Settings.Pro.bundle/About.plist"
+plutil -insert "items.1" -xml "<dict><key>cell</key><string>About</string><key>label</key><string>XXTouch Editor
+v${BVER}</string><key>value</key><string>https://www.xxtouch.com
+2016-2017 © XXTouch Team.
+All Rights Reserved.</string></dict>" "Settings.bundle/About.plist"
+plutil -insert "items.1" -xml "<dict><key>cell</key><string>About</string><key>label</key><string>XXTouch
+v${BVER}</string><key>value</key><string>https://www.xxtouch.com
+2016-2017 © XXTouch Team.
+All Rights Reserved.</string></dict>" "Settings.Pro.bundle/About.plist"
+
+# 5
+plutil -remove "items.6" "Settings.bundle/About.plist"
+plutil -remove "items.6" "Settings.Pro.bundle/About.plist"
+plutil -insert "items.6" -xml "<dict><key>cell</key><string>Button</string><key>label</key><string>Mail Feedback</string><key>action</key><string>SendMail:</string><key>args</key><dict><key>subject</key><string>[Feedback] XXTouch Editor v${BVER}</string><key>toRecipients</key><array><string>bug@xxtouch.com</string></array><key>ccRecipients</key><array><string>i.82@qq.com</string></array></dict></dict>" "Settings.bundle/About.plist"
+plutil -insert "items.6" -xml "<dict><key>cell</key><string>Button</string><key>label</key><string>Mail Feedback</string><key>action</key><string>SendMail:</string><key>args</key><dict><key>subject</key><string>[Feedback] XXTouch v${BVER}</string><key>toRecipients</key><array><string>bug@xxtouch.com</string></array><key>ccRecipients</key><array><string>i.82@qq.com</string></array></dict></dict>" "Settings.Pro.bundle/About.plist"
+
+# 6
+plutil -remove "PreferenceSpecifiers.6" "Settings.bundle/About.plist"
+plutil -remove "PreferenceSpecifiers.6" "Settings.Pro.bundle/About.plist"
+plutil -insert "PreferenceSpecifiers.6" -xml "<dict><key>Type</key><string>PSTitleValueSpecifier</string><key>DefaultValue</key><string>${BVER}</string><key>Title</key><string>Version</string><key>Key</key><string>sbVersion</string></dict>" "Settings.bundle/About.plist"
+plutil -insert "PreferenceSpecifiers.6" -xml "<dict><key>Type</key><string>PSTitleValueSpecifier</string><key>DefaultValue</key><string>${BVER}</string><key>Title</key><string>Version</string><key>Key</key><string>sbVersion</string></dict>" "Settings.Pro.bundle/About.plist"
