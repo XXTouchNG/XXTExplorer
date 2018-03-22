50eb936f9b525466e6e3e172154d89b654f94f39
Lessica <i.82@me.com>
2018-03-23 02:09:33 +0800

    Add DSYM uploader

--- build.sh
+++ build.sh
@@ -8,22 +8,29 @@ fi
 PACKAGE_NAME="${1:-latest}"
 DAEMON_VERSION="${2}"
 
+SRC_DIR="Releases/${PACKAGE_NAME}.xcarchive"
+DEST_DIR="Releases/${PACKAGE_NAME}"
+
 # 0
 echo "Update adapter..."
 ./adapter_encode.sh
 
 # 1
 echo "Bump version..."
-./bump_version.sh XXTExplorer-Archive ${DAEMON_VERSION}
+./bump_version.sh "XXTExplorer-Archive" ${DAEMON_VERSION}
 
 # 2
 echo "Trigger build..."
-xcodebuild archive -workspace XXTExplorer.xcworkspace -scheme XXTExplorer-Archive -archivePath Releases/${PACKAGE_NAME} | xcpretty --color
+xcodebuild archive -workspace "XXTExplorer.xcworkspace" -scheme "XXTExplorer-Archive" -archivePath "${DEST_DIR}" | xcpretty --color
 
 # 3
 echo "Trigger export..."
-xcodebuild -exportArchive -archivePath Releases/${PACKAGE_NAME}.xcarchive -exportPath Releases/${PACKAGE_NAME} -exportOptionsPlist DefaultExportOptions.plist -allowProvisioningUpdates | xcpretty --color
+xcodebuild -exportArchive -archivePath "${SRC_DIR}" -exportPath "${DEST_DIR}" -exportOptionsPlist "DefaultExportOptions.plist" -allowProvisioningUpdates | xcpretty --color
 
 # 4
+echo "Upload symbols..."
+./Libraries/BuglydSYMUploader/dSYMUpload.sh "abe3aa1f98" "d133a6f9-a23a-480c-a47a-e105191fd84c" "com.xxtouch.XXTExplorer" "${DAEMON_VERSION}" "${SRC_DIR}" "${DEST_DIR}" 1
+
+# 5
 echo "Succeed."
 open Releases
