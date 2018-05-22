189a42d7265f7aaf0723f9a9776bf12a3024eb76
Lessica <i.82@me.com>
2018-05-22 19:45:23 +0800

    Add Null: action for XUI Button

---
+++ XXTExplorer/Detail/XUIExtensions/ButtonAction/XXTEUIViewController+Null.h
@@ -0,0 +1,17 @@
+//
+//  XXTEUIViewController+Null.h
+//  XXTExplorer
+//
+//  Created by Zheng on 2018/5/22.
+//  Copyright © 2018 Zheng. All rights reserved.
+//
+
+#import "XXTEUIViewController.h"
+
+@class XUIButtonCell;
+
+@interface XXTEUIViewController (Null)
+
+- (NSNumber *)xui_Null:(XUIButtonCell *)cell;
+
+@end
---
+++ XXTExplorer/Detail/XUIExtensions/ButtonAction/XXTEUIViewController+Null.m
@@ -0,0 +1,19 @@
+//
+//  XXTEUIViewController+Null.m
+//  XXTExplorer
+//
+//  Created by Zheng on 2018/5/22.
+//  Copyright © 2018 Zheng. All rights reserved.
+//
+
+#import "XXTEUIViewController+Null.h"
+
+@class XUIButtonCell;
+
+@implementation XXTEUIViewController (Null)
+
+- (NSNumber *)xui_Null:(XUIButtonCell *)cell {
+    return @(0);
+}
+
+@end
